a3042925fa84254c6178825f35e2c3bb5ed38004
panqj <panqj01@mit.edu>
2024-03-16 23:59:27 -0400

    Initial Commit

---
+++ TextQuery/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  TextQuery
+//
+//  Created by panqj on 3/16/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
